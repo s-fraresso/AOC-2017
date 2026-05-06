6569c931bae4c2d76680c79d8ca080513252eabc
unknown <sylvainfraresso@laposte.net>
2026-05-06 12:02:42 +0200

    day5 part2

--- src/day5.py
+++ src/day5.py
@@ -15,4 +15,25 @@ def part1(input_file):
     return nb_step
 
 
+def part2(input_file):
+    with open(input_file, 'r') as f:
+        maze = [int(line.strip()) for line in f.readlines()]
+    
+    nb_step = 0
+    current_index = 0
+
+    while 0 <= current_index < len(maze):
+        step = maze[current_index]
+        if step >= 3:
+            maze[current_index] -= 1
+        else:
+            maze[current_index] += 1
+        
+        current_index += step
+        nb_step += 1
+
+    return nb_step
+
+
 print(part1("input\\day5.txt"))
+print(part2("input\\day5.txt"))
