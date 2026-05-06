a5f7e26b4b26763d2b14ab69f6a372592c95fdcc
unknown <sylvainfraresso@laposte.net>
2026-05-06 11:36:53 +0200

    day2 part2

--- src/day2.py
+++ src/day2.py
@@ -15,6 +15,22 @@ def part1(input_file):
             checksum += max_elt - min_elt
 
     return checksum
+
+
+def part2(input_file):
+    checksum = 0
+
+    with open(input_file, 'r') as f:
+        for line in f:
+            row = list(map(int, line.split()))
+
+            for i in range(len(row)):
+                for j in range(len(row)):
+                    if i != j and row[i] % row[j] == 0:
+                        checksum += row[i] // row[j]
+    
+    return checksum
         
 
 print(part1("input\\day2.txt"))
+print(part2("input\\day2.txt"))
