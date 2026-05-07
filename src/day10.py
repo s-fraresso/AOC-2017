15860fdf638a46aa7717e2285ea87499a98aaa35
unknown <sylvainfraresso@laposte.net>
2026-05-07 16:22:35 +0200

    day10 part2

--- src/day10.py
+++ src/day10.py
@@ -1,22 +1,45 @@
-SIZE = 256
+def tie_knot(circle, lengths, pos, skip):
+    N = len(circle)
+
+    for length in lengths:
+        for i in range(length // 2):
+            circle[(pos + i) % N], circle[(pos + length - 1 - i) % N] = circle[(pos + length - 1 - i) % N], circle[(pos + i) % N]
+        
+        pos = (pos + length + skip) % N
+        skip = (skip + 1) % N
+
+    return pos, skip
 
 
 def part1(input_file):
     with open(input_file, 'r') as f:
         lengths = list(map(int, f.readline().strip().split(",")))
 
-    circle = list(range(SIZE))
-    pos = 0
-    skip = 0
-
-    for length in lengths:
-        for i in range(length // 2):
-            circle[(pos + i) % SIZE], circle[(pos + length - 1 - i) % SIZE] = circle[(pos + length - 1 - i) % SIZE], circle[(pos + i) % SIZE]
-        
-        pos += length + skip
-        skip += 1
+    circle = list(range(256))
+    tie_knot(circle, lengths, 0, 0)
 
     return circle[0] * circle[1]
 
 
+def part2(input_file):
+    with open(input_file, 'r') as f:
+        lengths = list(map(ord, f.readline().strip())) + [17, 31, 73, 47, 23]
+
+    circle = list(range(256))
+    pos = skip = 0
+    for _ in range(64):
+        pos, skip = tie_knot(circle, lengths, pos, skip)
+
+    knot_hash = ""
+    for i in range(16):
+        cur_dense = circle[16 * i]
+        for j in range(1, 16):
+            cur_dense ^= circle[16 * i + j]
+        
+        knot_hash += hex(cur_dense)[2:].zfill(2)
+
+    return knot_hash
+
+
 print(part1("input\\day10.txt"))
+print(part2("input\\day10.txt"))
