ed71fe4cfd775fdf8a3b21c9013d989cb21ca41a
unknown <sylvainfraresso@laposte.net>
2026-05-07 15:18:22 +0200

    day8 part2

--- src/day8.py
+++ src/day8.py
@@ -17,4 +17,24 @@ def part1(input_file):
     return max(registers.values())
 
 
+def part2(input_file):
+    registers = defaultdict(int)
+    max_seen_value = 0
+
+    with open(input_file, "r") as f:
+        for line in f:
+            instruction, condition = line.strip().split(" if ")
+            modified_reg, action, added_value = instruction.split(" ")
+            checked_reg, check = condition.split(" ", maxsplit=1)
+ 
+            if eval("registers[\"" + checked_reg + "\"] " + check):
+                sign = 1 if action == "inc" else -1
+                registers[modified_reg] += sign * int(added_value)
+
+                max_seen_value = max(max_seen_value, registers[modified_reg])
+    
+    return max_seen_value
+
+
 print(part1("input\\day8.txt"))
+print(part2("input\\day8.txt"))
