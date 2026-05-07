7e69c5b24718a380d057e48e91da0d6e4e16554a
unknown <sylvainfraresso@laposte.net>
2026-05-07 15:33:54 +0200

    day9 part2

--- src/day9.py
+++ src/day9.py
@@ -29,4 +29,31 @@ def part1(input_file):
     return total_score
 
 
+def part2(input_file):
+    with open(input_file, 'r') as f:
+        stream = f.readline().strip()
+
+    nb_garbage = 0
+
+    i = 0
+    is_garbage = False
+    is_cancelled = False
+    while i < len(stream):
+        if is_cancelled:
+            is_cancelled = False
+        elif stream[i] == '!':
+            is_cancelled = True
+        elif is_garbage:
+            if stream[i] == '>':
+                is_garbage = False
+            else:
+                nb_garbage += 1
+        elif stream[i] == '<':
+            is_garbage = True
+        i += 1
+
+    return nb_garbage
+
+
 print(part1("input\\day9.txt"))
+print(part2("input\\day9.txt"))
