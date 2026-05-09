bf66b7865f3dd47f7783fbe84c1e29b335b29d3b
unknown <sylvainfraresso@laposte.net>
2026-05-09 13:14:12 +0200

    day3 part2

--- src/day3.py
+++ src/day3.py
@@ -1,4 +1,5 @@
 from math import sqrt, ceil
+from collections import defaultdict
 
 
 def part1(puzzle_input):
@@ -21,4 +22,37 @@ def part1(puzzle_input):
     return -1
 
 
+def sum_neighbours(i, j, grid):
+    total = 0
+
+    for i2 in (i - 1, i, i + 1):
+        for j2 in (j - 1, j, j + 1):
+            total += grid[(i2, j2)]
+
+    return total
+
+
+def part2(puzzle_input):
+    i, j = 1, 1
+    grid = defaultdict(int)
+    grid[(0, 0)] = 1
+    side_length = 3
+
+    while True:
+        for di, dj in ((-1, 0), (0, -1), (1, 0), (0, 1)):
+            for _ in range(side_length - 1):
+                i += di
+                j += dj
+
+                grid[(i, j)] = sum_neighbours(i, j, grid)
+
+                if grid[(i, j)] > puzzle_input:
+                    return grid[(i, j)]
+
+        side_length += 2
+        i += 1
+        j += 1
+
+
 print(part1(265149))
+print(part2(265149))
