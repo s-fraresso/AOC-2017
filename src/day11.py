ab62258e96a0cd446b990f29ec970bc3e691ffea
unknown <sylvainfraresso@laposte.net>
2026-05-09 12:38:00 +0200

    day11 part2

--- src/day11.py
+++ src/day11.py
@@ -6,6 +6,10 @@ MOVEMENT = {'n' : (0, -1 ,1),
             'nw': (-1, 0, 1)}
 
 
+def steps_from_origin(q, r, s):
+    return max(abs(q), abs(r), abs(s))
+
+
 def part1(input_file):
     with open(input_file, 'r') as f:
         directions = f.readline().strip().split(',')
@@ -17,7 +21,25 @@ def part1(input_file):
         r += move[1]
         s += move[2]
 
-    return max(abs(q), abs(r), abs(s))
+    return steps_from_origin(q, r, s)
+
+
+def part2(input_file):
+    with open(input_file, 'r') as f:
+        directions = f.readline().strip().split(',')
+
+    max_steps = 0
+    q = r = s = 0
+    for dir in directions:
+        move = MOVEMENT[dir]
+        q += move[0]
+        r += move[1]
+        s += move[2]
+
+        max_steps = max(max_steps, steps_from_origin(q, r, s))
+
+    return max_steps
 
 
 print(part1("input\\day11.txt"))
+print(part2("input\\day11.txt"))
