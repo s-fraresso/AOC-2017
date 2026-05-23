ce5ca5246224e26009a6eb5f8a6a168f5a045bb6
unknown <sylvainfraresso@laposte.net>
2026-05-23 17:36:20 +0200

    day15 part1

---
+++ src/day15.py
@@ -0,0 +1,15 @@
+def part1(seed_A, seed_B):
+    nb_matches = 0
+    A_value, B_value = seed_A, seed_B
+
+    for _ in range(40_000_000):
+        A_value = (A_value * 16807) % 2147483647
+        B_value = (B_value * 48271) % 2147483647
+
+        if A_value % 2**16 == B_value % 2**16:
+            nb_matches += 1
+
+    return nb_matches
+
+
+print(part1(783, 325))
