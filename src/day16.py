74d428b9ce755a03e71efeeadde2666e41cb39e6
unknown <sylvainfraresso@laposte.net>
2026-05-25 15:58:04 +0200

    day16 part2

--- src/day16.py
+++ src/day16.py
@@ -1,11 +1,8 @@
 NB_PROG = 16
 
 
-def part1(input_file):
-    with open(input_file, 'r') as f:
-        dance_moves = f.readline().strip().split(',')
-    
-    programs = list(map(chr, range(ord("a"), ord("a") + NB_PROG)))
+def dance_once(dance_moves, program_string):
+    programs = list(program_string)
     start_index = 0
 
     for move in dance_moves:
@@ -22,7 +19,24 @@ def part1(input_file):
             i, j = int(programs.index(p1)), int(programs.index(p2))
             programs[i], programs[j] = programs[j], programs[i]
     
-    return "".join(programs[start_index:] + programs[:start_index])
+    return "".join(programs[start_index:] + programs[:start_index])  
+
+
+def part1(input_file):
+    with open(input_file, 'r') as f:
+        dance_moves = f.readline().strip().split(',')
+    
+    
+    program_string = "".join(map(chr, range(ord("a"), ord("a") + NB_PROG)))
+    seen_strings = list()
+
+    while program_string not in seen_strings:
+        seen_strings.append(program_string)
+        program_string = dance_once(dance_moves, program_string)
+
+    return seen_strings[1_000_000_000 % len(seen_strings)]
+
+
 
 
 print(part1("input\\day16.txt"))
