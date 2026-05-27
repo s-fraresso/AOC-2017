023e61b74d8a462594880cbe66a88ba3c0ee972e
unknown <sylvainfraresso@laposte.net>
2026-05-28 00:03:36 +0200

    day17 part2

--- src/day17.py
+++ src/day17.py
@@ -10,4 +10,20 @@ def part1(nb_steps):
     return buffer[(pos + 1) % len(buffer)]
 
 
+def part2(nb_steps):
+    value_after_zero = None
+    pos = 0
+
+    for next_value in range(1, 50_000_001):
+        pos = (pos + nb_steps) % next_value
+
+        if pos == 0:
+            value_after_zero = next_value
+            
+        pos += 1
+    
+    return value_after_zero
+
+
 print(part1(345))
+print(part2(345))
